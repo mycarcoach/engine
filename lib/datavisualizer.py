9086acd118b5d8110279810af0ef731e9e779d89
André Kovac <me@andrekovac.com>
2019-03-09 20:48:32 +0100

    Pass on the events

--- lib/datavisualizer.py
+++ lib/datavisualizer.py
@@ -6,25 +6,26 @@ import threading
 import queue
 import time
 
-client = None
+dataQueue = queue.Queue()
 
 def new_client(newClient, server):
     client = newClient
-    server.send_message_to_all("Hey all, a new client has joined us")
+    server.send_message_to_all("Connection established")
 
     while True:
-        server.send_message(newClient, "hallo mein Freund")
-        time.sleep(1)
+        if not dataQueue.empty():
+            print('>>>>>> push new data to frontend')
+            server.send_message(newClient, dataQueue.get())
+        time.sleep(0.05)
 
 class DataVisualizer:
 
     def __init__(self):
-        self.dataQueue = queue.Queue()
         t = threading.Thread(target=self.thread)
         t.start()
 
     def pushDataset(self, data):
-        self.dataQueue.put(data)
+        dataQueue.put(data)
 
     def thread(self):
         server = WebsocketServer(7254, host='127.0.0.1')
