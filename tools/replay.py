4ca3ad654d5277550b16b42b7600486d64ba8e70
Elia Bieri <contact@eliabieri.com>
2019-03-09 20:48:37 +0100

    added json to replay tool

--- tools/replay.py
+++ tools/replay.py
@@ -1,23 +1,30 @@
 import paho.mqtt.client as mqtt
 import time
-
-def on_connect(client, userdata, flags, rc):
-    print("CONNECTED!")
+import json
+def main():
+    def on_connect(client, userdata, flags, rc):
+        print("CONNECTED!")
+        
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.connect("46.101.168.60", 1883, 60)
+    client.loop_start()
     
-client = mqtt.Client()
-client.on_connect = on_connect
-client.connect("46.101.168.60", 1883, 60)
-client.loop_start()
+    f = open("/Users/eliabieri/Downloads/testDrive.csv")
+    for line in f.readlines():
+        if line.startswith(","):
+                #skip first line
+            continue
+        values = line.split(",")
+        speed = json.dumps('{"signal":{"definition":{"name":"speed","unit":"Unit_PerCent","description":"in km/h"},"measurement":{"value":{' + values[1] + '},"utc":1550761385086735000}}')
+        break_ = json.dumps('{"signal":{"definition":{"name":"speed","unit":"Unit_PerCent","description":"in km/h"},"measurement":{"value":{' + values[2] + '},"utc":1550761385086735000}}')
+        accel = json.dumps('{"signal":{"definition":{"name":"speed","unit":"Unit_PerCent","description":"in km/h"},"measurement":{"value":{' + values[3] + '},"utc":1550761385086735000}}')
+        client.publish("/value/ESP_v_Signal", speed, qos=0, retain=False)
+        client.publish("/value/ESP_Bremsdruck", break_, qos=0, retain=False)
+        client.publish("/value/ESP_Laengsbeschl", accel, qos=0, retain=False)
+        time.sleep(0.01)
+    
+    client.loop_stop()
 
-f = open("/Users/eliabieri/Downloads/testDrive.csv")
-for line in f.readlines():
-    if line.startswith(","):
-            #skip first line
-        continue
-    values = line.split(",")
-    client.publish("/value/ESP_v_Signal", values[1], qos=0, retain=False)
-    client.publish("/value/ESP_Bremsdruck", values[2], qos=0, retain=False)
-    client.publish("/value/ESP_Laengsbeschl", values[3], qos=0, retain=False)
-    time.sleep(0.01)
-
-client.loop_stop()
+if __name__=="__main__":
+    main()
