8ac6e29afca512f51acefd39602d7574f9fbc726
André Kovac <me@andrekovac.com>
2019-03-09 12:11:53 +0100

    Move dataprovider

--- lib/dataprovider.py
+++ lib/dataprovider.py
@@ -10,29 +10,43 @@ class DataProvider:
     def __init__(self, dataProviderSource, dataProviderConfiguration):
         self.dataProviderSource = dataProviderSource
         self.dataProviderConfiguration = dataProviderConfiguration
-        print(self.dataProviderConfiguration.getConfiguration())
-        asyncio.get_event_loop().run_until_complete(self.handler())
-    
+        self.data = '{ "foo": "bar" }'
+
     async def handler(self):
-        async with websockets.connect(f'ws://{self.dataProviderSource.value}:8765') as websocket:
+        async with websockets.connect('ws://130.82.239.210/ws') as websocket:
+            print("Inside socket")
             # set initial configuration
             await websocket.send(self.dataProviderConfiguration.getConfiguration())
-            self.data = await websocket.recv()
-    
+            print(f"data sent: {self.dataProviderConfiguration.getConfiguration()}")
+            await asyncio.sleep(1)
+            try:
+                self.data = await websocket.recv()
+            except asyncio.streams.IncompleteReadError:
+                pass
+            except websockets.exceptions.ConnectionClosed:
+                pass
+            else:
+                print("Data returned:")
+                print(self.data)
+            finally:
+                print("The 'try except' is finished")
+
     def getData(self):
+        asyncio.get_event_loop().
+        asyncio.get_event_loop().run_until_complete(self.handler())
         return json.loads(self.data)
-            
+
 
 class DataProviderSource(Enum):
     SIMULATION = "130.82.239.210"
     REAL = "192.168.8.10"
 
 class DataProviderConfiguration:
-    
+
     def __init__(self, signals, samplerate, withtimestamp):
         self.signals = signals
         self.samplerate = samplerate
         self.withtimestamp = withtimestamp
-    
+
     def getConfiguration(self):
         return json.dumps({'signals': self.signals, 'samplerate': self.samplerate, 'withtimestamp': self.withtimestamp})
