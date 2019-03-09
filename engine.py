da4f0631777f1ea14136345e3eb694ffb491c11a
Elia Bieri <contact@eliabieri.com>
2019-03-09 01:55:03 +0100

    fixed setup

--- engine.py
+++ engine.py
@@ -2,5 +2,9 @@
 import dataprovider
 
 if __name__ == "__main__":
-    dataProvider = dataProvider.DataProvider()
+    dataProviderSource = dataprovider.DataProviderSource.SIMULATION
+    dataProviderConfiguration = dataprovider.DataProviderConfiguration(["MO_Drehzahl_01"], 250, True)
+    dataProvider = dataprovider.DataProvider(dataProviderSource, dataProviderConfiguration)
+    while 1:
+        print(dataProvider.getData())
     
