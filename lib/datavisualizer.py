8e05c9a5555a51115de94a250da8ca20175371c3
André Kovac <me@andrekovac.com>
2019-03-09 19:03:24 +0100

    Successful connection to websocket

--- lib/datavisualizer.py
+++ lib/datavisualizer.py
@@ -1,31 +1,30 @@
 import asyncio
 import datetime
 import random
-import websocket
+from websocket_server import WebsocketServer
 import threading
 import queue
 import time
 
+client = None
+
+def new_client(newClient, server):
+    client = newClient
+    server.send_message_to_all("Hey all, a new client has joined us")
+
 class DataVisualizer:
-    
+
     def __init__(self):
         self.dataQueue = queue.Queue()
         t = threading.Thread(target=self.thread)
         t.start()
-        
+
     def pushDataset(self, data):
         self.dataQueue.put(data)
-    
+
     def thread(self):
-        print("Starting visualizer thread...")
-        websocket.enableTrace(True)
-        ws = websocket.WebSocketApp("ws://127.0.0.0:5678",
-                              on_open=self.on_open)
-        ws.run_forever()
-    
-    def on_open(self, ws):
-        while True:
-            if not self.dataQueue.empty():
-                print("pushed data to ws client")
-                ws.send(self.dataQueue.get())
-            time.sleep(0.1)
+        server = WebsocketServer(7254, host='127.0.0.1')
+        # server.send_message('127.0.0.1', )
+        server.set_fn_new_client(new_client)
+
+        server.run_forever()
