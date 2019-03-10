a69a08f9edc9b3e33d9a4f7a84454cb6ee41c9e9
André Kovac <me@andrekovac.com>
2019-03-10 11:16:39 +0100

    Update app design

--- app/connectToStream.js
+++ app/connectToStream.js
@@ -1,4 +1,5 @@
 (function() {
+
   // Data visualization
   var ctx = document.getElementById("speedChart");
   var config = {
@@ -29,9 +30,10 @@
       responsive: true,
       title: {
         display: true,
-        text: "How close are you to the optimal braking curve?",
+        text: "Instant Feedback",
         fontFamily: "'Ubuntu', sans-serif",
-        fontSize: 20,
+        fontSize: 36,
+        fontColor: "white",
       },
       tooltips: {
         mode: "index",
@@ -40,6 +42,13 @@
       hover: {
         mode: "nearest",
         intersect: true
+      },
+      legend: {
+        labels: {
+          fontSize: 16,
+          fontFamily: "'Ubuntu', sans-serif",
+          fontColor: "white",
+        }
       },
       scales: {
         xAxes: [
@@ -51,7 +60,8 @@
             scaleLabel: {
               display: true,
               labelString: "Time",
-              fontSize: 20,
+              fontSize: 30,
+              fontColor: "white",
         fontFamily: "'Ubuntu', sans-serif",
             },
             ticks: {
@@ -63,16 +73,21 @@
           {
             display: true,
             gridLines: {
+              display: false,
               lineWidth: 2,
+              color: "#efefef",
             },
             ticks: {
+              display: false,
               fontSize: 18,
               fontFamily: "'Ubuntu', sans-serif",
+              fontColor: "white",
             },
             scaleLabel: {
               display: true,
-              labelString: "Speed",
-              fontSize: 24,
+              labelString: "Acceleration",
+              fontSize: 30,
+              fontColor: "white",
               fontFamily: "'Ubuntu', sans-serif",
 
             }
@@ -84,19 +99,25 @@
 
   window.speedChart = new Chart(ctx, config);
 
+  var timer = 10;
+  var hasData = false;
+
   // Web Socket
   var ws = new WebSocket("ws://127.0.0.1:7254/");
   console.log("Websocket connect started!");
+
   ws.onopen = function(event) {
-    console.log("Connected successfully!")
+    console.log("Connected successfully!");
   }
   ws.onerror = function(event) {
-    console.log("Error occurred")
-    console.log(event.data)
+    console.log("Error occurred");
+    console.log(event.data);
   }
   ws.onmessage = function(event) {
-    console.log("Got new message")
+    console.log("Got new message");
     var data = JSON.parse(event.data);
+    hasData = true;
+    window.setTimeout(() => { hasData = false; console.log('time is over'); }, 10000);
 
     var optimalData = [];
     var actualData = [];
@@ -110,15 +131,20 @@
     var newActualDataset = {
       data: actualData,
       label: "Actual",
-      borderColor: "red",
-      fill: false
+      borderColor: "#282078",
+      fill: false,
+      pointRadius: 0,
+      borderWidth: 5,
     };
 
     var newIdealDataset = {
       data: optimalData,
       label: "Optimal",
-      borderColor: "blue",
-      fill: false
+      borderColor: "#e4e4e4",
+      fill: false,
+      pointRadius: 0,
+      borderWidth: 5,
+      borderDash: [10, 3],
     };
 
     config.data.labels = labels;
